a383159945ab9ecc027bf429269d62c596b98647
Bart Marseille <bart.marseille@gmail.com>
2022-11-27 18:26:52 +0100

    changed parameters and variables type to dict

--- utils/logistic_map_utils.py
+++ utils/logistic_map_utils.py
@@ -1,5 +1,8 @@
 # import numpy as np
 
+from typing import Callable
+
+
 def logistic_map(r, x):
     """The logistic map models the evolution of a population,
     by mapping the current population size `x_t` to the next `x_t+1`$,
@@ -14,6 +17,35 @@ def logistic_map(r, x):
 
    """
     return r * x * (1 - x)
+
+
+def iterate(map: Callable, r, x0=[], n=0) -> list:
+
+    assert isinstance(r, (list, int, float)), 'r must be of type list, int or float'
+    assert isinstance(x0, (list, int, float)), 'x0 must be of type list, int or float'
+
+    # make sure r and x0 are in array
+    r_list = [r] if isinstance(r, (int, float)) else r
+    x0_list = [x0] if isinstance(x0, (int, float)) else x0
+    
+    trajectories = []
+    for r in r_list:
+        for x0 in x0_list:
+            trajectory = [x0]
+            x = x0
+            for i in range(n):
+                x = map(r, x)
+                trajectory.append(x)
+            t = (trajectory, r, n)
+            trajectories.append(t)
+
+    if len(trajectories)==0:
+        return ([], r, 0)
+    elif len(trajectories)==1:
+        return trajectories[0]
+    else:
+        return trajectories
+
 
 #rename to convergence
 def get_fixed_points(r, n_equilibration_steps=1000, n_sampling_steps=100, epsilon=0.0001, verbose=False):
