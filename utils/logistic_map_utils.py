ffce0b0971bb2aa9043e6483a5e53de8c8ff760d
Bart Marseille <bart.marseille@gmail.com>
2022-12-07 12:39:13 +0100

    refactoring and cleaning up code

--- utils/logistic_map_utils.py
+++ utils/logistic_map_utils.py
@@ -1,17 +1,6 @@
 import numpy as np
 
-# def logistic_map(P: dict, V: dict) -> dict:
-#     """The implementation of a relative population system with:
-#     -  parameters `P: ['r'] and,
-#     -  variables `V: ['x']`,
-#     that maps to the output variable v_hat['x']`
-#     """
-#     r = P['r']  # reproduction rate
-#     x = V['x']  # relative population size
 
-#     x_hat = r * x * (1 - x)
-
-#     return {'x': x_hat}
 def logistic_map(V: np.ndarray, r=3.0) -> np.ndarray:
     """The implementation of a relative population system with:
     -  variables `V`,
@@ -55,16 +44,14 @@ def logistic_map_fixed_points(P, n_equilibration_steps=1000, n_sampling_steps=10
         fixed_points = [0.6623452662682413]
     else:  # Periodic or chaotic
         x = 0.5
-        V = x
 
         # First, we run 1000 iteration to give the system a chance to settle
         for i in range(n_equilibration_steps):
-            V = logistic_map(V, **P)
+            x = logistic_map(x, **P)
 
         # Determine number of unique points we encounter in the subsequent `n_sampling_steps`
         for i in range(n_sampling_steps):
-            V = logistic_map(V, **P)
-            x = V
+            x = logistic_map(x, **P)
 
             # Check whether, up to numerical accuracy, our new $x$ is already in this array.
             x_in_fixed_points = False
